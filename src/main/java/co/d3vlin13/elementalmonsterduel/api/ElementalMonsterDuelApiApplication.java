8d4c749dee0a6bf5c21e34d07146b5543f623f3f
Alexis Duque <46509440+D3vlin@users.noreply.github.com>
2026-08-27 13:44:29 -0500

    Proyect init

---
+++ src/main/java/co/d3vlin13/elementalmonsterduel/api/ElementalMonsterDuelApiApplication.java
@@ -0,0 +1,13 @@
+package co.d3vlin13.elementalmonsterduel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElementalMonsterDuelApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElementalMonsterDuelApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/d3vlin13/elementalmonsterduel/api/ElementalMonsterDuelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.d3vlin13.elementalmonsterduel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElementalMonsterDuelApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
