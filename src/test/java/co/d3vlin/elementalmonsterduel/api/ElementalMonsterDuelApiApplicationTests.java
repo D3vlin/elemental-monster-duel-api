7a8e4cefc21d7fccfe0b32021bc3d9ca579bed5f
Alexis Duque <46509440+D3vlin@users.noreply.github.com>
2026-09-07 06:04:35 -0500

    chore: Edit test

--- src/test/java/co/d3vlin/elementalmonsterduel/api/ElementalMonsterDuelApiApplicationTests.java
+++ src/test/java/co/d3vlin/elementalmonsterduel/api/ElementalMonsterDuelApiApplicationTests.java
@@ -1,9 +1,11 @@
-package co.d3vlin13.elementalmonsterduel.api;
+package co.d3vlin.elementalmonsterduel.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ElementalMonsterDuelApiApplicationTests {
 
     @Test
