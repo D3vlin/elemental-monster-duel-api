3230de01d807f46f815dba965d5a3e236f775367
Alexis Duque <46509440+D3vlin@users.noreply.github.com>
2026-09-07 06:17:37 -0500

    chore: Add profile properties

--- src/main/java/co/d3vlin/elementalmonsterduel/api/ElementalMonsterDuelApiApplication.java
+++ src/main/java/co/d3vlin/elementalmonsterduel/api/ElementalMonsterDuelApiApplication.java
@@ -1,9 +1,16 @@
-package co.d3vlin13.elementalmonsterduel.api;
+package co.d3vlin.elementalmonsterduel.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "co.d3vlin.elementalmonsterduel.entity")
+@ComponentScan(basePackages = {
+        "co.d3vlin.elementalmonsterduel.api",
+        "co.d3vlin.elementalmonsterduel.mapper",
+})
 public class ElementalMonsterDuelApiApplication {
 
     public static void main(String[] args) {
